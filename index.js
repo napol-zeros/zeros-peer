f98f2f5d80523388a7e94923fffdb2d0c54d478c
zeros-origin <auctok@gmail.com>
2021-07-02 21:37:15 +0700

    Revise cli

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-/** Zeros Origin CLI. */
-
-const chalk = require( 'chalk' );
-const clear = require( 'clear' );
-const figlet = require( 'figlet' );
-
-require( './modules/main' );
-require( './modules/files' );
-const command = require( './modules/command' );
-
-clear();
-
-console.log(
-  chalk.green(
-    figlet.textSync( 'ZerosPeer', { horizontalLayout: 'default' } )
-  )
-);
+#!/usr/bin/env node
 
 console.log('CLI version 0.0.1\n');
-
-const run = async () => {
-    const credentials = await command.askCommand();
-    console.log( credentials );
-}
-
-run();
